8f681112ee9ca5d543fbb0578e297da892c5bba7
NIMA MD 1V ❤️🙃 <nimsaranimsara704@gmail.com>
2025-05-27 15:08:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const path = require('path');
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
+
+// Modules
+let server = require('./qr');   // QR related routes
+let code = require('./pair');  // Pairing code routes
+
+// Increase max listeners to avoid warnings (optional)
 require('events').EventEmitter.defaultMaxListeners = 500;
+
+// Use routes
 app.use('/server', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+app.use('/code', code);   // <-- Make sure pairing route is under /code
+
+// Serve static HTML files
+app.get('/pair', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'pair.html'));
+});
+
+app.get('/qr', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'qr.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'main.html'));
+});
+
+// Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Start server
 app.listen(PORT, () => {
-    console.log(`
-Don't Forgot To Give Star
+    console.log(`Server running on http://localhost:${PORT}`);
+});
 
- Server running on http://localhost:` + PORT)
-})
-
-module.exports = app
+module.exports = app;
